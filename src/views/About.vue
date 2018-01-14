050a5113dc09189dc7e4f938f340e1fda9241d54
cjh <1418503647@qq.com>
2018-01-14 13:22:57 +0800

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,41 @@
+<template>
+    <my-page title="关于">
+        <div class="about-box">
+            <img class="logo" src="/static/img/logo.png">
+            <div class="version">计数器 v1.1.0</div>
+            <div class="copyright">© 2018 <a href="https://yunser.com">云设</a></div>
+        </div>
+    </my-page>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+            }
+        },
+        mounted() {
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .about-box {
+        text-align: center;
+        .logo {
+            display: block;
+            width: 64px;
+            margin: 80px auto 16px auto;
+        }
+        .version {
+            margin-bottom: 16px;
+            color: #999;
+        }
+        .copyright {
+            text-align: center;
+            a {
+                color: inherit;
+            }
+        }
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+    <my-page title="计数器">
+        <section class="input-box">
+            <div class="count">{{ count }}</div>
+            <div class="btns">
+                <ui-raised-button label=" + " primary @click="add"/>
+            </div>
+            <div class="btns">
+                <ui-raised-button label=" - " @click="minus"/>
+                <ui-raised-button label="重置" @click="reset"/>
+            </div>
+        </section>
+    </my-page>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                count: 0
+            }
+        },
+        methods: {
+            add: function () {
+                this.count++
+            },
+            minus: function () {
+                this.count--
+                if (this.count < 0) {
+                    this.count = 0
+                }
+            },
+            reset: function () {
+                this.count = 0
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .input-box {
+        .count {
+            margin-bottom: 16px;
+            color: #666;
+            font-size: 80px;
+            text-align: center;
+        }
+        .btns {
+            text-align: center;
+            margin-bottom: 16px;
+        }
+        .add-btn {
+            font-size: 40px;
+            width: 88px;
+        }
+    }
+</style>
